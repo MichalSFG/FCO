93c3f3e404d036459af060b5a13bc7fd78af928d
MichalSFG <mickeyjas80@gmail.com>
2020-11-13 16:32:58 +0100

    api testing

---
+++ src/main/java/pl/jasmichal/controller/FlightController.java
@@ -0,0 +1,30 @@
+package pl.jasmichal.controller;
+
+import ch.qos.logback.classic.Logger;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FlightController {
+
+    private final Logger logger = (Logger) LoggerFactory.getLogger(FlightController.class);
+
+    @GetMapping
+    public String getFlightNumber() {
+        return "home";
+    }
+
+    @PostMapping
+    public String getFlightData(@RequestParam long flightNumber) {
+        logger.info("FR " + flightNumber);
+        String url = "http://aviation-edge.com/v2/public/flights?key=07cf31-be1c81&flightIata=FR" + flightNumber;
+        return "redirect:" + url;
+
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<form method="post">
+    Podaj numer lotu: FR<label>
+    <input type="number" name="flightNumber">
+</label><br>
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
